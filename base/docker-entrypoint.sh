ba45d67c0977262c1f1c4a1fa6f71597362e4e25
Samuel Chen <samuel.net@gmail.com>
2014-08-23 04:26:08 +0800

    start supervisord foreground

--- base/docker-entrypoint.sh
+++ base/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/supervisord 
+/usr/bin/supervisord -n
--- utils/start-base.sh
+++ utils/start-base.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run -c 1 -m 128m -d --name="base" -p 10022:22 -p 19001:9001 -v ../log:/var/log -v ../data:/data  samuelchen/base 
+CWD=~/dockerifles
+
+docker run -c 1 -m 128m -d --name="base" -p 10022:22 -p 19001:9001 -v $CWD/log:/var/log -v $CWD/data:/data  samuelchen/base 
