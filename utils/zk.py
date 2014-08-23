33dce8a70f00a7bd34ed9c2f7d1a6db7f2d3b10b
Samuel Chen <samuel.net@gmail.com>
2014-08-23 13:23:21 +0800

    .

--- utils/zk.py
+++ utils/zk.py
@@ -11,15 +11,18 @@ log = logging.getLogger('root')
 log.info = lambda x: sys.stdout.write(x + '\n')
 log.error = lambda x: sys.stderr.write(x+'\n')
 
-_cwd = os.getcwd()
+_cwd = '~/dockerfiles' #os.getcwd()
 _mem = 128
 _cpu = 1
 _name = 'node'
 _ports = [22, 9001, 2181]
 _repository = 'samuelchen/zookeeper'
+_data = _cwd + '/data'
+_log = _cwd + '/log'
+_myid = '/etc/zookeeper/conf/myid'
 
 # nodes count
-count = 3
+count = 1
 if len(sys.argv) > 1:
 	try:
 		count = int(sys.argv[1])
@@ -29,21 +32,28 @@ if len(sys.argv) > 1:
 
 log.info('You are staring %d zookeeper nodes' % count)
 
-nodenames = map(lambda x: '%s%d' % (_name, x), range(count))
+nodenames = map(lambda x: '%s%d' % (_name, x), xrange(count))
 nodeports = {}
 temp_ports = map(lambda x: x+10000, _ports)
 for host in nodenames:
 	nodeports[host] = temp_ports
 
 	script = 'docker run -c %d -m %dm --name="%s" ' % (_cpu, _mem, host)
-	for i in range(count):
+	for i in range(len(_ports)):
 		script = script + ' -p %d:%d ' % (nodeports[host][i], _ports[i])
-		log.info('exposing container port %d to host port %d' % (nodeports[host][i], _ports[i]))
-	script = script + _repository
+		#log.info('exposing container port %d to host port %d' % (nodeports[host][i], _ports[i]))
+	vol_data = _data + '/' + host
+	vol_log = _log + '/' + host
+
+	script += ' -v %s:/docker-data -v %s:/docker-log ' % (vol_data, vol_log)
+	script +=  _repository
+
+
+	log.info(script)
+	sys.command('docker rm %s' % hostname)
+	status, out = commands.getstatusoutput(script)
+	log.error('status: %d' % status)
+	log.error(out)
 
 	temp_ports = map(lambda x: x+1, temp_ports)
-	log.info(script)
-	#status, out = commands.getstatusoutput(script)
-	#log.error(status)
-	#log.error(out)
 
